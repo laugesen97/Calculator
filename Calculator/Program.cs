0283daa7fa55051a8e630f421112266ebc7c9bfb
Asbjørn Schöneberg Krogh <au651267@uni.au.dk>
2021-02-04 13:51:50 +0100

    Update Program.cs

--- Calculator/Program.cs
+++ Calculator/Program.cs
@@ -7,6 +7,8 @@ namespace Calculator
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+
+            Console.WriteLine("Hello Verden");
         }
     }
 }
